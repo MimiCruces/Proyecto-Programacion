f1411123ea28c42b413d57552075e4efaa42419e
MimiCruces <noemicruces18@aragon.unam.mx>
2023-11-28 19:33:44 -0600

    Se corrigo un error en la conexion a la Bd en el apartado de Eliminar

--- respuestaEliminar.php
+++ respuestaEliminar.php
@@ -1,19 +1,19 @@
 <?php
 require "conexionBD.php";
 mysqli_set_charset($conexion,'utf8');
-$usuario = $_POST['id'];
+$id = $_POST['id'];
 $password = $_POST['contraseña'];
 $q = "SELECT COUNT(*) AS contar FROM usuario WHERE id= '$id' AND password = '$password'";
 $consulta = mysqli_query($conexion,$q);
 $array = mysqli_fetch_array($consulta);
 
 if($array['contar']>0){
-    $registroEliminado =$_POST['usuario'];
+    $registroEliminado =$_POST['id'];
     $consulta1="DELETE FROM usuario WHERE id= ";
     mysqli_query($conexion, $consulta1 . $registroEliminado);
     mysqli_close($conexion);
     echo "Eliminado";
 }else{
-    echo "Revise sus credenciales" 
+    echo "Revise sus credenciales";
 }
 ?>
